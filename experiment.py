c235e1ba6e2db7536621277fe8b54037335af755
Hardik Bansal <hardikbansal24@gmail.com>
2017-04-16 18:56:56 +0530

    Added the Fake pool support

--- experiment.py
+++ experiment.py
@@ -28,6 +28,7 @@ h1_size = 150
 h2_size = 300
 z_size = 100
 batch_size = 1
+pool_size = 5
 sample_size = 10
 ngf = 128
 ndf = 64
@@ -96,6 +97,13 @@ def error_fake_pool(fake_pool_rec, num_fakes):
     else:
         return tf.reduce_mean(tf.squared_difference(fake_pool_rec[pool_size - num_fakes - 1:],1))
 
+def add_fake_sample(fake_pool, fake_image,num_fakes):
+    if(num_fakes < pool_size):
+        fake_pool[num_fakes,] = fake_image
+    else:
+        if(np.random.rand(1)[0] > 0.5):
+            randidx = np.random.randint(0,pool_size,1)[0]
+            fake_pool[randidx,] = fake_image
 
 def train():
 
@@ -126,7 +134,9 @@ def train():
     
     fake_pool_A = tf.placeholder(tf.float32, [pool_size, img_width, img_height, img_layer], name="fake_pool_A")
     fake_pool_B = tf.placeholder(tf.float32, [pool_size, img_width, img_height, img_layer], name="fake_pool_B")
-    num_fake_inputs = tf.placeholder(tf.float32, [], name="num_fake_inputs")
+
+    num_fake_inputs_A = 0
+    num_fake_inputs_B = 0
 
     lr = tf.placeholder(tf.float32, shape=[], name="lr")
 
@@ -149,19 +159,19 @@ def train():
         fake_pool_rec_B = build_gen_discriminator(fake_pool_B, "d_B")
 
 
-        # Loss functions for various things
+    # Loss functions for various things
 
     cyc_loss = tf.reduce_mean(tf.squared_difference(input_A, cyc_A)) + tf.reduce_mean(tf.squared_difference(input_B, cyc_B))
     
 
-    disc_loss_A = tf.reduce_mean(tf.square(rec_A)) + tf.reduce_mean(tf.squared_difference(fake_rec_A,1))
-    disc_loss_B = tf.reduce_mean(tf.square(rec_B)) + tf.reduce_mean(tf.squared_difference(fake_rec_B,1))
+    disc_loss_A = tf.reduce_mean(tf.squared_difference(fake_rec_A,1))
+    disc_loss_B = tf.reduce_mean(tf.squared_difference(fake_rec_B,1))
     
-    g_loss_A = cyc_loss + disc_loss_A
-    g_loss_B = cyc_loss + disc_loss_B
+    g_loss_A = cyc_loss + disc_loss_B
+    g_loss_B = cyc_loss + disc_loss_A
 
-    d_loss_A = tf.reduce_mean(tf.square(rec_A)) + error_fake_pool(fake_pool_rec_A, num_fake_inputs)
-    d_loss_B = tf.reduce_mean(tf.square(rec_B)) + error_fake_pool(fake_pool_rec_B, num_fake_inputs)
+    d_loss_A = tf.reduce_mean(tf.square(rec_A)) + error_fake_pool(fake_pool_rec_A, num_fake_inputs_A)
+    d_loss_B = tf.reduce_mean(tf.square(rec_B)) + error_fake_pool(fake_pool_rec_B, num_fake_inputs_B)
 
     
     optimizer = tf.train.AdamOptimizer(lr)
@@ -200,6 +210,11 @@ def train():
         images_A = []
         images_B = []
 
+        fake_images_A = np.empty([pool_size,img_height, img_width, img_layer])
+        fake_images_B = np.empty([pool_size,img_height, img_width, img_layer])
+        num_fakes_A = 0
+        num_files_B = 0
+
         for i in range(10): 
             image_tensor = sess.run(image_A)
             images_A.append(image_tensor)
@@ -229,10 +244,12 @@ def train():
                 B_input = images_B[j]
                 A_input = tf.reshape(A_input,[batch_size,img_height, img_width, img_layer]).eval(session=sess)
                 B_input = tf.reshape(B_input,[batch_size,img_height, img_width, img_layer]).eval(session=sess)
-                sess.run([g_A_trainer,fake_A],feed_dict={input_A:A_input, input_B:B_input, lr:curr_lr})
-                sess.run(d_A_trainer,feed_dict={input_A:A_input, input_B:B_input, lr:curr_lr})
-                sess.run(g_B_trainer,feed_dict={input_A:A_input, input_B:B_input, lr:curr_lr})
-                sess.run(d_B_trainer,feed_dict={input_A:A_input, input_B:B_input, lr:curr_lr})
+                _, fake_A_temp = sess.run([g_A_trainer,fake_A],feed_dict={input_A:A_input, input_B:B_input, lr:curr_lr})
+                add_fake_sample(fake_images_A, fake_A_temp[0], num_fake_inputs_A)
+                sess.run(d_A_trainer,feed_dict={input_A:A_input, input_B:B_input, lr:curr_lr, fake_pool_A:fake_images_A})
+                # num_fake_inputs_A+=1
+                # sess.run(g_B_trainer,feed_dict={input_A:A_input, input_B:B_input, lr:curr_lr})
+                # sess.run(d_B_trainer,feed_dict={input_A:A_input, input_B:B_input, lr:curr_lr})
 
             # if(i % 10 == 0):
             #     saver.save(sess,"/output/cyleganmodel")
