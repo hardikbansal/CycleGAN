cab857434002dfe766e03f0ab8bab79253ca72a2
Hardik Bansal <hardikbansal24@gmail.com>
2017-04-22 13:55:06 +0530

    Added the layers and summary ops

--- experiment.py
+++ experiment.py
@@ -8,6 +8,8 @@ import os
 import shutil
 from PIL import Image
 import time
+import random
+
 
 from layers import *
 
@@ -77,6 +79,36 @@ def build_generator_resnet_6blocks(inputgen, name="generator"):
 
     return out_gen
 
+def build_generator_resnet_9blocks(inputgen, name="generator"):
+    with tf.variable_scope(name):
+        f = 7
+        ks = 3
+        
+        pad_input = tf.pad(inputgen,[[0, 0], [ks, ks], [ks, ks], [0, 0]], "REFLECT")
+        o_c1 = general_conv2d(pad_input, ngf, f, f, 1, 1, 0.02,name="c1")
+        o_c2 = general_conv2d(o_c1, ngf*2, ks, ks, 2, 2, 0.02,"SAME","c2")
+        o_c3 = general_conv2d(o_c2, ngf*4, ks, ks, 2, 2, 0.02,"SAME","c3")
+
+        o_r1 = build_resnet_block(o_c3, ngf*4, "r1")
+        o_r2 = build_resnet_block(o_r1, ngf*4, "r2")
+        o_r3 = build_resnet_block(o_r2, ngf*4, "r3")
+        o_r4 = build_resnet_block(o_r3, ngf*4, "r4")
+        o_r5 = build_resnet_block(o_r4, ngf*4, "r5")
+        o_r6 = build_resnet_block(o_r5, ngf*4, "r6")
+        o_r7 = build_resnet_block(o_r6, ngf*4, "r7")
+        o_r8 = build_resnet_block(o_r7, ngf*4, "r8")
+        o_r9 = build_resnet_block(o_r8, ngf*4, "r9")
+
+        o_c4 = general_deconv2d(o_r9, [batch_size,128,128,ngf*2], ngf*2, ks, ks, 2, 2, 0.02,"SAME","c4")
+        o_c5 = general_deconv2d(o_c4, [batch_size,256,256,ngf], ngf, ks, ks, 2, 2, 0.02,"SAME","c5")
+        o_c6 = general_conv2d(o_c5, img_layer, f, f, 1, 1, 0.02,"SAME","c6",do_relu="False")
+
+        # Adding the tanh layer
+
+        out_gen = tf.nn.tanh(o_c6,"t1")
+
+
+    return out_gen
 
 
 def build_gen_discriminator(inputdisc, name="discriminator"):
@@ -95,18 +127,6 @@ def build_gen_discriminator(inputdisc, name="discriminator"):
     return out_disc
 
 
-def add_fake_sample(fake_pool, fake_image, num_fakes):
-    if(num_fakes < pool_size):
-        if(num_fakes == 0):
-            fake_pool[0] = fake_image[0]
-        else:
-            # print("Over Here")
-            fake_pool = np.append(fake_pool, np.array([fake_image]), axis=0)
-    else:
-        if(np.random.rand(1)[0] > 0.5):
-            randidx = np.random.randint(0,pool_size,1)[0]
-            fake_pool[randidx,] = fake_image
-    # print(fake_pool.shape)
 
 def train():
 
@@ -143,8 +163,8 @@ def train():
     lr = tf.placeholder(tf.float32, shape=[], name="lr")
 
     with tf.variable_scope("Model") as scope:
-        fake_B = build_generator_resnet_6blocks(input_A, name="g_A")
-        fake_A = build_generator_resnet_6blocks(input_B, name="g_B")
+        fake_B = build_generator_resnet_9blocks(input_A, name="g_A")
+        fake_A = build_generator_resnet_9blocks(input_B, name="g_B")
         rec_A = build_gen_discriminator(input_A, "d_A")
         rec_B = build_gen_discriminator(input_B, "d_B")
 
@@ -152,8 +172,8 @@ def train():
 
         fake_rec_A = build_gen_discriminator(fake_A, "d_A")
         fake_rec_B = build_gen_discriminator(fake_B, "d_B")
-        cyc_A = build_generator_resnet_6blocks(fake_B, "g_B")
-        cyc_B = build_generator_resnet_6blocks(fake_A, "g_A")
+        cyc_A = build_generator_resnet_9blocks(fake_B, "g_B")
+        cyc_B = build_generator_resnet_9blocks(fake_A, "g_A")
 
         scope.reuse_variables()
 
@@ -163,7 +183,7 @@ def train():
 
     # Loss functions for various things
 
-    cyc_loss = tf.reduce_mean(tf.squared_difference(input_A, cyc_A)) + tf.reduce_mean(tf.squared_difference(input_B, cyc_B))
+    cyc_loss = tf.reduce_mean(tf.abs(input_A-cyc_A)) + tf.reduce_mean(tf.abs(input_B-cyc_B))
     
 
     disc_loss_A = tf.reduce_mean(tf.squared_difference(fake_rec_A,1))
@@ -193,12 +213,12 @@ def train():
 
     # Summary Variables
 
-    tf.scalar_summary("g_A_loss", g_loss_A)
-    tf.scalar_summary("g_B_loss", g_loss_B)
-    tf.scalar_summary("d_A_loss", d_loss_A)
-    tf.scalar_summary("d_B_loss", d_loss_B)
+    tf.summary.scalar("g_A_loss", g_loss_A)
+    tf.summary.scalar("g_B_loss", g_loss_B)
+    tf.summary.scalar("d_A_loss", d_loss_A)
+    tf.summary.scalar("d_B_loss", d_loss_B)
 
-    summary_op = tf.merge_all_summaries()
+    summary_op = tf.summary.merge_all()
     
 
 
@@ -224,16 +244,18 @@ def train():
 
         fake_images_A = np.zeros((pool_size,img_height, img_width, img_layer))
         fake_images_B = np.zeros((pool_size,img_height, img_width, img_layer))
-        num_fakes_A = 0
-        num_files_B = 0
 
-        for i in range(10): 
+        A_input = np.zeros((max_images,batch_size,img_height, img_width, img_layer))
+        B_input = np.zeros((max_images,batch_size,img_height, img_width, img_layer))
+
+        for i in range(max_images): 
             image_tensor = sess.run(image_A)
-            images_A.append(image_tensor)
+            A_input[i] = image_tensor.reshape((batch_size,img_height, img_width, img_layer))
 
-        for i in range(10):
+        for i in range(max_images):
             image_tensor = sess.run(image_B)
-            images_B.append(image_tensor)
+            B_input[i] = image_tensor.reshape((batch_size,img_height, img_width, img_layer))
+
 
         coord.request_stop()
         coord.join(threads)
@@ -242,7 +264,10 @@ def train():
 
         writer = tf.summary.FileWriter("/output/2")
 
-        for epoch in range(0,1):
+        # a,b,c,d,e = sess.run([cyc_loss,disc_loss_A,disc_loss_B,g_loss_A,g_loss_B],feed_dict={input_A:A_input[0], input_B:B_input[0], fake_pool_A:fake_images_A, fake_pool_B:fake_images_B})
+        # print(a,b,c,d,e)
+
+        for epoch in range(0,10):
             print ("In the epoch ", epoch)
 
             if(epoch < 100) :
@@ -250,8 +275,9 @@ def train():
             else:
                 curr_lr = 0.0002 - 0.0002*(epoch-100)/100
 
-            A_input = tf.reshape(images_A[0],[batch_size,img_height, img_width, img_layer]).eval(session=sess)
-            B_input = tf.reshape(images_B[0],[batch_size,img_height, img_width, img_layer]).eval(session=sess)
+            summary_str = sess.run(summary_op,feed_dict={input_A:A_input[0], input_B:B_input[0], fake_pool_A:fake_images_A, fake_pool_B:fake_images_B})
+            writer.add_summary(summary_str, epoch)
+
 
             for ptr in range(0,max_images):
 
@@ -259,23 +285,38 @@ def train():
 
                 print(time.time()*1000.0)
 
-                # print(time.time()*1000.0)
+                if(num_fake_inputs < pool_size):
                 
-                _, fake_B_temp = sess.run([g_A_trainer,fake_B],feed_dict={input_A:A_input, input_B:B_input, lr:curr_lr})
-                sess.run(d_B_trainer,feed_dict={input_A:A_input, input_B:B_input, lr:curr_lr, fake_pool_B:fake_images_B[0:num_fake_inputs+1]})
-                
-                _, fake_A_temp = sess.run([g_B_trainer, fake_A],feed_dict={input_A:A_input, input_B:B_input, lr:curr_lr})
-                sess.run(d_A_trainer,feed_dict={input_A:A_input, input_B:B_input, lr:curr_lr, fake_pool_A:fake_images_A[0:num_fake_inputs+1]})
+                    _, fake_B_temp = sess.run([g_A_trainer,fake_B],feed_dict={input_A:A_input[0], input_B:B_input[0], lr:curr_lr})
+                    sess.run(d_B_trainer,feed_dict={input_A:A_input[0], input_B:B_input[0], lr:curr_lr, fake_pool_B:fake_images_B[0:num_fake_inputs+1]})
+                    
+                    _, fake_A_temp = sess.run([g_B_trainer, fake_A],feed_dict={input_A:A_input[0], input_B:B_input[0], lr:curr_lr})
+                    sess.run(d_A_trainer,feed_dict={input_A:A_input[0], input_B:B_input[0], lr:curr_lr, fake_pool_A:fake_images_A[0:num_fake_inputs+1]})
+            
+                    # summary_str = sess.run(summary_op,feed_dict={input_A:A_input[ptr], input_B:B_input[ptr], fake_pool_A:fake_images_A, fake_pool_B:fake_images_B})
+                else :
+                    _, fake_B_temp = sess.run([g_A_trainer,fake_B],feed_dict={input_A:A_input[ptr], input_B:B_input[ptr], lr:curr_lr})
+                    sess.run(d_B_trainer,feed_dict={input_A:A_input[ptr], input_B:B_input[ptr], lr:curr_lr, fake_pool_B:fake_images_B})
+                    
+                    _, fake_A_temp = sess.run([g_B_trainer, fake_A],feed_dict={input_A:A_input[ptr], input_B:B_input[ptr], lr:curr_lr})
+                    sess.run(d_A_trainer,feed_dict={input_A:A_input[ptr], input_B:B_input[ptr], lr:curr_lr, fake_pool_A:fake_images_A})
 
+                    # summary_str = sess.run(summary_op,feed_dict={input_A:A_input[ptr], input_B:B_input[ptr], fake_pool_A:fake_images_A, fake_pool_B:fake_images_B})
                 
 
-                # summary_str = sess.run(summary_op,feed_dict={input_A:A_input, input_B:B_input, fake_pool_A:fake_images_A[0:num_fake_inputs+1], fake_pool_B:fake_images_B[0:num_fake_inputs+1]})
                 # writer.add_summary(summary_str, epoch*max_images + ptr)
 
                 if(num_fake_inputs < pool_size):
                     fake_images_A[num_fake_inputs] = fake_A_temp[0]
                     fake_images_B[num_fake_inputs] = fake_B_temp[0]
                     num_fake_inputs+=1
+                else :
+                    p = random.random()
+                    if p > 0.5:
+                        random_id = random.randint(0,pool_size-1)
+                        fake_images_A[random_id] = fake_A_temp[0]
+                        random_id = random.randint(0,pool_size-1)
+                        fake_images_B[random_id] = fake_B_temp[0]
 
                 
 
--- layers.py
+++ layers.py
@@ -10,6 +10,11 @@ def lrelu(x, leak=0.2, name="lrelu", alt_relu_impl=True):
         else:
             return tf.maximum(x, leak*x)
 
+def instance_norm(x):
+    epsilon = 1e-9
+    mean, var = tf.nn.moments(x, [1, 2], keep_dims=True)
+    return tf.div(tf.subtract(x, mean), tf.sqrt(tf.add(var, epsilon)))
+
 
 def general_conv2d(inputconv, o_d=64, f_h=7, f_w=7, s_h=1, s_w=1, stddev=0.02, padding="VALID", name="conv2d", do_norm=True, do_relu=True, relufactor=0):
     with tf.variable_scope(name):
@@ -19,11 +24,12 @@ def general_conv2d(inputconv, o_d=64, f_h=7, f_w=7, s_h=1, s_w=1, stddev=0.02, p
         biases = tf.get_variable('b',[o_d],initializer=tf.constant_initializer(0.0))
         conv = tf.nn.bias_add(conv,biases)
         if do_norm:
-            dims = conv.get_shape()
-            scale = tf.get_variable('scale',[dims[1],dims[2],dims[3]],initializer=tf.constant_initializer(1))
-            beta = tf.get_variable('beta',[dims[1],dims[2],dims[3]],initializer=tf.constant_initializer(0))
-            conv_mean,conv_var = tf.nn.moments(conv,[0])
-            conv = tf.nn.batch_normalization(conv,conv_mean,conv_var,beta,scale,0.001)
+            conv = instance_norm(conv)
+            # dims = conv.get_shape()
+            # scale = tf.get_variable('scale',[dims[1],dims[2],dims[3]],initializer=tf.constant_initializer(1))
+            # beta = tf.get_variable('beta',[dims[1],dims[2],dims[3]],initializer=tf.constant_initializer(0))
+            # conv_mean,conv_var = tf.nn.moments(conv,[0])
+            # conv = tf.nn.batch_normalization(conv,conv_mean,conv_var,beta,scale,0.001)
         if do_relu:
             if(relufactor == 0):
                 conv = tf.nn.relu(conv,"relu")
@@ -31,6 +37,8 @@ def general_conv2d(inputconv, o_d=64, f_h=7, f_w=7, s_h=1, s_w=1, stddev=0.02, p
                 conv = lrelu(conv, relufactor, "lrelu")
 
     return conv
+
+
 
 def general_deconv2d(inputconv, outshape, o_d=64, f_h=7, f_w=7, s_h=1, s_w=1, stddev=0.02, padding="VALID", name="deconv2d", do_norm=True, do_relu=True, relufactor=0):
     with tf.variable_scope(name):
@@ -40,11 +48,12 @@ def general_deconv2d(inputconv, outshape, o_d=64, f_h=7, f_w=7, s_h=1, s_w=1, st
         biases = tf.get_variable('b',[o_d],initializer=tf.constant_initializer(0.0))
         conv = tf.nn.bias_add(conv,biases)
         if do_norm:
-            dims = conv.get_shape()
-            scale = tf.get_variable('scale',[dims[1],dims[2],dims[3]],initializer=tf.constant_initializer(1))
-            beta = tf.get_variable('beta',[dims[1],dims[2],dims[3]],initializer=tf.constant_initializer(0))
-            conv_mean,conv_var = tf.nn.moments(conv,[0])
-            conv = tf.nn.batch_normalization(conv,conv_mean,conv_var,beta,scale,0.001)
+            conv = instance_norm(conv)
+            # dims = conv.get_shape()
+            # scale = tf.get_variable('scale',[dims[1],dims[2],dims[3]],initializer=tf.constant_initializer(1))
+            # beta = tf.get_variable('beta',[dims[1],dims[2],dims[3]],initializer=tf.constant_initializer(0))
+            # conv_mean,conv_var = tf.nn.moments(conv,[0])
+            # conv = tf.nn.batch_normalization(conv,conv_mean,conv_var,beta,scale,0.001)
         if do_relu:
             if(relufactor == 0):
                 conv = tf.nn.relu(conv,"relu")
